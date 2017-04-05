43a0554bb330173185eb343113a1b5c35ae0909c
CHNnoodle <625887616@qq.com>
2017-04-05 19:21:08 +0800

    monitortime

--- etl_monitor.py
+++ etl_monitor.py
@@ -2,22 +2,25 @@
 
 import os
 import commands
+import datetime
 
 if __name__=='__main__':
         print 'etl_monitor'
+        print '当前时间 ' + datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
         oscmd = 'ps -ef | grep python | grep etl_judge.py | grep -v grep'
         (res_status,res_output) = commands.getstatusoutput(oscmd)
         if res_output :
                 print res_output
                 print 'etl调度正常，进程无需重启'
         else :
-                os.chdir("/root/etl/")
+                print 'etl调度中断，重启调度程序'
+                os.chdir("/root/PyEtl/")
                 pycmd = 'nohup stdbuf -oL python etl_judge.py >>etl.log 2>>$(date +%Y%m%d-%H-%M).err &'
                 osres = os.popen(pycmd)
                 (os_status,os_output) = commands.getstatusoutput(oscmd)
                 if os_output :
                         print os_output
-                        print 'etl调度已启动'
+                        print 'etl调度启动成功'
                 else :
                         print 'etl进程启动失败'
         print '退出etl_monitor'
