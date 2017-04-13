8e90514d710295336a9807cc3dd2b53176cbe021
CHNnoodle <625887616@qq.com>
2017-04-13 14:42:30 +0800

    1
    1

--- etl_dbtotxt.py
+++ etl_dbtotxt.py
@@ -9,11 +9,12 @@ def dbtotxt(sql,filename,hdfs_path,local_path = '/root/dbdata/'):
         cursor = conn.cursor()
         cursor.execute(sql)
         res = cursor.fetchall()
-        
+        cursor.close()
+        conn.close()
         filepath = local_path+filename+'.txt'
         with open(filepath, 'wb+') as f :
             for row_data in res:
-                f.write(''.join(list(row_data)).encode('utf-8')+'\n')
+                f.write(''.join(row_data).encode('utf-8')+'\n')
 
     except Exception, e:
         print e
@@ -22,6 +23,6 @@ def dbtotxt(sql,filename,hdfs_path,local_path = '/root/dbdata/'):
 if __name__ == '__main__':
 
     sql = '''select ACCT_DAY||'|*|'||USERNAME||'|*|'||FULLNAME||'|*|'||
-    CERTIFICATION_NO||'|*|'||MOBILE||'|*|'||STATUS||'|*|'||CREATETIME from STAGE_LOG.T_USERINFO'''
+    CERTIFICATION_NO||'|*|'||MOBILE||'|*|'||STATUS||'|*|'||CREATETIME from STAGE_LOG.T_USERINFO WHERE ROWNUM<=10'''
     dbtotxt(sql,'test','1','/Users/wanggang/Downloads/')
 
