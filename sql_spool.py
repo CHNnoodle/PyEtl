0454d99059528dfff2850d6ac2c72a874447ebaa
CHNnoodle <625887616@qq.com>
2017-05-12 11:31:06 +0800

    11

--- sql_spool.py
+++ sql_spool.py
@@ -1,13 +1,10 @@
 # coding:utf8
 
-
-import os
-import logging
 import cx_Oracle
 import hdfs
 
 
-def put_hdfs(dns, sql, filename, hdfs_path, local_path='/root/spooldata/'):
+def put_hdfs(dns, sql, filename,local_path='/root/spooldata/'):
     try:
         count = ''
         with cx_Oracle.connect(dns) as conn:
@@ -16,9 +13,9 @@ def put_hdfs(dns, sql, filename, hdfs_path, local_path='/root/spooldata/'):
                 cursor.execute(sql)
                 res = cursor.fetchall()
                 count = cursor.rowcount
-                logging.info('%s:OJDBC导出%s行数据' % (filename, count))
+                print '%s:OJDBC导出%s行数据' % (filename, count)
             except Exception, e:
-                logging.error(e)
+                print e
                 raise Exception(e)
 
             finally:
@@ -28,29 +25,11 @@ def put_hdfs(dns, sql, filename, hdfs_path, local_path='/root/spooldata/'):
         with open(filepath, 'wb+') as f:
             for row_data in res:
                 f.write(''.join(row_data).encode('utf-8') + '\n')
-            logging.info('写入本地文件%s完成' % filepath)
+            print '写入本地文件%s完成' % filepath
 
-        try:
-            logging.info('上传数据到hdfs')
-            txt_hdfs_path = hdfs_path + filename
-            client = hdfs.Client("http://192.10.86.101:50070",
-                                 root="/", timeout=100, session=False)
-            if filename[16:22]!='v_acct':
-                client.delete(hdfs_path, recursive=True)
-            client.upload(txt_hdfs_path, filepath)
-            logging.info('upload数据完成')
-        except Exception, e:
-            logging.error(e)
-            raise Exception(e)
-
-        oscmd = 'rm -f ' + filepath
-        logging.info('删除本地文件')
-        res = os.system(oscmd)
-
-        (retcode, retinfo) = ('success', '')
     except Exception, e:
         (retcode, retinfo) = ('fail', e)
-        logging.error(e)
+        print e
         raise Exception('fail', e)
 
     finally:
@@ -67,6 +46,6 @@ if __name__ == '__main__':
        '|*|' || MAC || '|*|' || number2ip(nas_ip) || '|*|' || SVLAN || '|*|' || PVLAN
   from STAGE_LOG.t_ACCT where ACCT_DAY='20170510' '''
     dns = r'xijia/dba#789@NJUST'
-    (x, y, z) = put_hdfs(dns, sql, '20170510', '1', '/Users/wanggang/Downloads/')
+    (x, y, z) = put_hdfs(dns, sql, '20170510', '/Users/wanggang/Downloads/')
     print x
     print y
